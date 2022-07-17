45a4074d8c6846b3e7a44808e7e51197aa2f48ee
Taha Kara <74018704+tahakara@users.noreply.github.com>
2022-07-18 01:29:45 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var audio = document.createElement("AUDIO")
 document.body.appendChild(audio);
 audio.src = "https://tahakara.dev/assets/lorem.mp3";
 audio.volume = 0.4;
-document.body.addEventListener("mousemove", function () {
+document.body.addEventListener("click", function () {
     audio.play()
 })
 
