8474a8999912a49ce63df2c7852e2cc1d13ba5b3
Taha Kara <74018704+tahakara@users.noreply.github.com>
2022-07-21 23:33:13 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,42 @@
 
 //  Lorem ipsum Song 
 var audio = document.createElement("AUDIO")
+
+
+
+
 document.body.appendChild(audio);
 audio.src = "https://tahakara.dev/assets/lorem.mp3";
 audio.volume = 0.4;
-// document.body.addEventListener("click", function () {
-//     audio.play()
-// })
 
-window.addEventListener('DOMContentLoaded', () => {
-       audio.play()
+
+function fade() {
+    var i = 0;
+    var image = document.getElementsByClassName("sence")[0];
+    image.style.opacity = 0;
+    
+    var k = window.setInterval(function() {
+      
+        if (i >= 200) {
+            clearInterval(k);
+        } else {
+            image.style.opacity = i / 100;
+            i++;
+        }
+    }, 100);
+};
+
+
+
+
+
+document.body.addEventListener("click", function () {
+ 
+    // image transition      .sence opaccity 0  to 1     transition: visibility 0s linear 300ms, opacity 6000ms; 
+    fade()
+    audio.play()
+
 })
+
+
 
